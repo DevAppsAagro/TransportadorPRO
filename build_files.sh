092ecf4fc05819aadc8a8b0de00f981026336f17
DevAppsAagro/pecuaria.app <devapp.apps@gmail.com>
2025-02-27 16:08:50 -0300

    Melhorar script de build para ser mais robusto

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 
-# Instala as dependências do projeto
-python3 -m pip install -r requirements.txt
+echo "Instalando dependências..."
+pip install --upgrade pip
+pip install -r requirements.txt
 
-# Coleta os arquivos estáticos
-python3 manage.py collectstatic --noinput
+echo "Coletando arquivos estáticos..."
+mkdir -p staticfiles
+python manage.py collectstatic --noinput
 
-# Executa as migrações (opcional, dependendo da configuração)
-# python3 manage.py migrate
+echo "Build concluído com sucesso!"
