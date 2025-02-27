0bdcf8043eb7519c504e4c013d6b8f9aea49f832
DevAppsAagro/pecuaria.app <devapp.apps@gmail.com>
2025-02-27 16:01:11 -0300

    Corrigir script de build para usar python3 e pip3 explicitamente

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Instala as dependências do projeto
-pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Coleta os arquivos estáticos
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Executa as migrações (opcional, dependendo da configuração)
-# python manage.py migrate
+# python3 manage.py migrate
